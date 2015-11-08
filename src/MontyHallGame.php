130e663aa644cc2180925195b5b9bfef7a9c5f59
Toby Mackenzie <public@tobymackenzie.com>
2015-11-08 14:48:12 -0500

    montyhall: switched from `rand()` to `mt_rand()` for better randomization

--- src/MontyHallGame.php
+++ src/MontyHallGame.php
@@ -20,13 +20,13 @@ class MontyHallGame{
 
 		//--game
 		//---put car behind door
-		$this->carDoor = rand(1,$this->doorCount);
+		$this->carDoor = mt_rand(1,$this->doorCount);
 		//---player selects door
-		$this->playerDoor = rand(1,$this->doorCount);
+		$this->playerDoor = mt_rand(1,$this->doorCount);
 		//---host selects door(s)
 		$randModifier = ($this->carDoor === $this->playerDoor) ? $this->doorCount - 1 : $this->doorCount - 2;
 		for($i = 0; $i < $this->hostDoorCount; ++$i){
-			$this->hostDoors[$i] = rand(1, $randModifier);
+			$this->hostDoors[$i] = mt_rand(1, $randModifier);
 			if($this->hostDoors[$i] === $this->playerDoor){
 				++$this->hostDoors[$i];
 			}
@@ -37,7 +37,7 @@ class MontyHallGame{
 		//---determine if player switch would move away from car
 		$this->switchedFromCar = ($this->carDoor === $this->playerDoor);
 		//---determine if player switch would move to car
-		$this->newPlayerDoor = rand(1, $this->doorCount - 2);
+		$this->newPlayerDoor = mt_rand(1, $this->doorCount - 2);
 		while($this->newPlayerDoor === $this->playerDoor || in_array($this->newPlayerDoor, $this->hostDoors)){
 			++$this->newPlayerDoor;
 		}
